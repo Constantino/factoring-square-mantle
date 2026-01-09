b3568338d95b556d802805131e6cfee0121aac03
Cristian Chaparro <cristianchaparroa@gmail.com>
2026-01-08 23:17:24 -0500

    ver: update solidity version and fix deploy all script

--- contracts/src/MockUSDC.sol
+++ contracts/src/MockUSDC.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -35,7 +35,7 @@ contract MockUSDC is ERC20, Ownable {
      * @param to Address to mint tokens to
      * @param amount Amount in USDC (will be multiplied by 10^6)
      */
-    function mintUSDC(address to, uint256 amount) external {
+    function mintUsdc(address to, uint256 amount) external {
         _mint(to, amount * 10 ** DECIMALS);
     }
 }
