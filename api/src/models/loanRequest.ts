97be99fe712c4249cb62cc466ad99357b7a49518
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2026-01-09 03:40:34 -0600

    added model for loanrequests

---
+++ api/src/models/loanRequest.ts
@@ -0,0 +1,33 @@
+export interface LoanRequestBody {
+    invoice_number: string;
+    invoice_amount: number;
+    invoice_due_date: string; // ISO date string (YYYY-MM-DD)
+    term: number;
+    customer_name: string;
+    delivery_completed: boolean;
+    advance_rate: number;
+    monthly_interest_rate: number;
+    max_loan: number;
+    not_pledged: boolean;
+    assignment_signed: boolean;
+    borrower_address: string;
+}
+
+export interface LoanRequest {
+    id: number;
+    created_at: Date;
+    modified_at: Date;
+    invoice_number: string;
+    invoice_amount: number;
+    invoice_due_date: Date;
+    term: number;
+    customer_name: string;
+    delivery_completed: boolean;
+    advance_rate: number;
+    monthly_interest_rate: number;
+    max_loan: number;
+    not_pledged: boolean;
+    assignment_signed: boolean;
+    borrower_address: string;
+}
+
